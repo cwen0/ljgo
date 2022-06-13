8c473c19a432c562844bf79b52c482365b453251
Cwen Yin <cwenyin0@gmail.com>
2022-06-13 19:32:16 +0800

    fix markdown parse
    Signed-off-by: Cwen Yin <cwenyin0@gmail.com>

--- app/util/markdown.go
+++ app/util/markdown.go
@@ -1,11 +1,29 @@
 package util
 
 import (
+	"bytes"
 	"html/template"
 
-	blackfriday "github.com/russross/blackfriday/v2"
+	mathjax "github.com/litao91/goldmark-mathjax"
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/extension"
+	"github.com/yuin/goldmark/parser"
+	"github.com/yuin/goldmark/renderer/html"
 )
 
 func ParseMarkdown(markdown string) template.HTML {
-	return template.HTML(blackfriday.Run([]byte(markdown)))
+	// return template.HTML(goldmark.Convert([]byte(markdown)))
+	md := goldmark.New(
+		goldmark.WithExtensions(extension.GFM),
+		goldmark.WithExtensions(mathjax.MathJax),
+		goldmark.WithParserOptions(
+			parser.WithAutoHeadingID(),
+		),
+		goldmark.WithRendererOptions(
+			html.WithHardWraps(),
+		),
+	)
+	var buf bytes.Buffer
+	_ = md.Convert([]byte(markdown), &buf)
+	return template.HTML(buf.String())
 }
--- go.mod
+++ go.mod
@@ -4,9 +4,10 @@ go 1.17
 
 require (
 	github.com/gorilla/feeds v1.1.1
-	github.com/russross/blackfriday/v2 v2.1.0
+	github.com/litao91/goldmark-mathjax v0.0.0-20210217064022-a43cf739a50f
 	github.com/urfave/cli v1.22.9
 	github.com/urfave/negroni v1.0.0
+	github.com/yuin/goldmark v1.4.12
 	gopkg.in/fsnotify.v1 v1.4.7
 	gopkg.in/yaml.v2 v2.4.0
 )
@@ -15,6 +16,7 @@ require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
